14360a62864866eb8a41ce5d28ffded12c8c1db9
Murphy <sydneyporter74@yahoo.com>
2020-11-11 16:48:05 -0500

    finished

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const seed = require('./seeds/index.js');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: true }).then(() => {
